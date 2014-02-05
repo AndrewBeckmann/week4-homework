6e20fae51916bcfff0cb9ce47a379d042136c546
AndrewBeckmann <abeckmann2014@kellogg.northwestern.edu>
2014-02-05 00:01:12 -0600

    HW Part 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Week4hw::Application.routes.draw do
 
  get "/weather/search" => "weather#search"
  get "/weather/conditions" => "weather#conditions"
+ get "/index" => "products#index"
+ get "/show" => "products#show"
   get "/" => "pages#home"
 
   # HINT: Add more routes here...
